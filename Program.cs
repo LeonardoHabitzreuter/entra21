610eeef2d2b42e4e7d4bb3a9b68304c131dfb71a
Entra21 <entra21@proway.treina>
2020-07-07 22:09:15 -0300

    conditionals and comparisions

--- Program.cs
+++ Program.cs
@@ -6,19 +6,27 @@ namespace primeira_aula
     {
         static void Main(string[] args)
         {
-            var myText = "meu texto";
-            var myNumber = 8;
-            var myPrice = 4.099;
-            var myBoolean = myPrice == myNumber;
-
-            Console.WriteLine("Você é maior de idade?");
+            Console.WriteLine("Qual é a sua idade?");
             var result = Console.ReadLine();
+            
+            // Transformando o texto que o usuário digitou para um tipo numérico
+            var userAge = Int32.Parse(result);
 
-            myBoolean = result == "sim";
+            // Comparando SE a idade do usuário é maior que 15 E menor que 51
+            if (userAge > 15 && userAge < 51)
+            {
+                Console.WriteLine("Sim, você tem idade para o Entra21");
+            }
+            else if (userAge == 14 || userAge == 15)
+            {
+                Console.WriteLine("Você não tem idade para o Entra21, mas tem para ser menor aprendiz");
+            }
+            else
+            {
+                Console.WriteLine("Não, você não tem idade nem para o Entra21 e nem para ser menor aprendiz");
+            }
 
-            Console.WriteLine(myText);
-            Console.WriteLine(myBoolean);
-            Console.WriteLine(myBoolean);
+            Console.WriteLine("Só isso, mais nada!");
         }
     }
 }
