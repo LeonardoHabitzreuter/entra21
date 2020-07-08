bdad1847f1bf8cf2772a4f768b9cca72f3f6c508
Entra21 <entra21@proway.treina>
2020-07-07 21:01:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myText = "meu texto";
+            var myNumber = 8;
+            var myPrice = 4.099;
+            var myBoolean = myPrice == myNumber;
+
+            Console.WriteLine("Você é maior de idade?");
+            var result = Console.ReadLine();
+
+            myBoolean = result == "sim";
+
+            Console.WriteLine(myText);
+            Console.WriteLine(myBoolean);
+            Console.WriteLine(myBoolean);
+        }
+    }
+}
